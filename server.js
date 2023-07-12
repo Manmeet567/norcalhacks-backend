cc31360dd52045de5630eaa1a0d2e3ffa5ecc23f
Manmeet567 <manmeet567singh@gmail.com>
2023-07-12 10:10:43 +0530

    created basic structure and connected to mongodb

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+const userRoutes = require('./routes/userRoutes');
+
+
+const app = express()
+
+const PORT = 4000 || process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+//middlewares
+app.use(express.json())
+app.use(cors());
+
+//routes
+app.use('/api/user', userRoutes);
+
+mongoose.set('strictQuery', true);
+mongoose.connect(MONGODB_URI)
+ .then(() => {
+    app.listen(PORT, () => {
+        console.log("Server Listening on PORT",PORT)
+    });
+ })
+ .catch((err) => {
+    console.log(err);
+  });
