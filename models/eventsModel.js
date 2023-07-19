fe89593264a8928a315ca0b016cc86357281a592
Manmeet567 <manmeet567singh@gmail.com>
2023-07-19 16:32:45 +0530

    create event model using mongoose

---
+++ models/eventsModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    startTime:{
+        type:Date,
+        required:true
+    },
+    endTime:{
+        type:Date,
+        required:true
+    }
+}, {timestamps:true})
+
+const Event = mongoose.model('Event', eventSchema)
+module.exports = Event
