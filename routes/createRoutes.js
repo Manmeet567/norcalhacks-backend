331ab2f86496c2dd27611918f7a96769ca964166
Manmeet567 <manmeet567singh@gmail.com>
2023-07-24 19:17:28 +0530

    created routes for recentevents, deleteevents etc.|

--- routes/createRoutes.js
+++ routes/createRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { createEvent, getEvents } = require('../controllers/createControllers');
+const { createEvent, getEvents, recentEvents, deleteEvent, locationEvent } = require('../controllers/createControllers');
 const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
@@ -12,5 +12,14 @@ router.post('/createEvent', createEvent)
 
 //get events route
 router.get('/getEvents', getEvents)
+
+// get recent 3 events
+router.get('/recentEvents', recentEvents)
+
+//delete a document
+router.delete('/deleteEvent/:_id', deleteEvent)
+
+//get docs from specific location
+router.get('/location', locationEvent)
 
 module.exports = router
